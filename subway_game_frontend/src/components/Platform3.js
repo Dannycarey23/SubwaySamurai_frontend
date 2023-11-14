6a73e9941ad1ba611d321234d2a722d97e93b018
Chamsron <emmacameron219@gmail.com>
2023-11-14 20:35:10 +0000

    platform 3

--- subway_game_frontend/src/components/Platform3.js
+++ subway_game_frontend/src/components/Platform3.js
@@ -1,22 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import { Navigate, redirect, useNavigate } from 'react-router-dom';
-import styles from './platform3.module.css'
-//import '../components/Platform3.css';
-  
+import styles from './platform3.module.css';
 
 const Platform3 = ({character, music}) => {
     
     const [name, setName] = useState(character.name)
     const [x, setX] = useState(0);
     
-    
     const Navigate = useNavigate();
 
     useEffect(() => {
       function moveCharacter(e) {
-        if (e.keyCode === 39) {
+        if (e.keyCode === 68) {
           setX((x) => x += 20);
-          } else if (e.keyCode === 37) {
+          } else if (e.keyCode === 65) {
             setX((x) => x -= 20);
           }
 
@@ -30,7 +27,7 @@ const Platform3 = ({character, music}) => {
 
       useEffect(() => {
         let newPage = '/room3'
-        if (x >= window.innerWidth){
+        if (x >= window.innerWidth -450){
           music.stop()
             Navigate(newPage)
       }}, [x])
@@ -40,11 +37,12 @@ const Platform3 = ({character, music}) => {
       }, [])
 
       return ( 
-        <div id="platform3div">
-          <h1>{name}</h1>
-          <img src = "assets/KendokaV2.png" height= "500px" id = "Sprite" style={{ marginLeft: `${x}px` }}></img>
-          <div class="bubble" contenteditable>You have chanced your luck twice so as they say, third times the charm. This time you make it 3 stops without breaking down and you make it to the glorious city centre. Before you can walk 5 feet you are greeted by the heathen also known as the Lord Provost. Half zombie half human (this is what you trained for!).
+        <div className={styles.platform3div}>
+          <div className={styles.bubble} contenteditable>
+            <p>You have chanced your luck twice so as they say, third times the charm. This time you make it 3 stops without breaking down and you make it to the glorious city centre. Before you can walk 5 feet you are greeted by the heathen also known as the Lord Provost. Half zombie half human (this is what you trained for!).</p>
           </div>
+
+          <img src = "assets/KendokaV2.png" height= "500px" className={styles.Sprite} style={{ marginLeft: `${x}px` }}></img>
         
         </div>
 
